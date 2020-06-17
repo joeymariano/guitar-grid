c39fa8bd8e3590208011903d91271e2b75b7a62a
joeymariano <joey.michalina.mariano@gmail.com>
2020-06-17 13:25:14 -0400

    iterate over notes

--- src/components/fretboard.js
+++ src/components/fretboard.js
@@ -5,8 +5,7 @@ import './fretboard.css'
 class Fretboard extends Component {
   constructor(props){
   	super(props)
-  	this.state = {  tuning: ['E2','A2', 'D3', 'G3', 'B3', 'E4'],
-                    frets: 24 }
+  	this.state = {  tuning: ['E2','A2', 'D3', 'G3', 'B3', 'E4'] }
   }
 
   renderStrings = (tuning) => {
