36e7e57983f3f268785f9c4119bde35dad9aef1a
joeymariano <joey.michalina.mariano@gmail.com>
2020-06-17 14:58:07 -0400

    added rudimentary header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
 import Fretboard from './components/fretboard'
+import Header from './components/header'
 
 function App() {
   return (
     <div className="App">
+    	<Header />
       <Fretboard/>
     </div>
   );
---
+++ src/components/header.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './header.css'
+
+function Header(){
+	return( <div class="header">
+						GUITAR GRID
+					</div> )
+}
+
+export default Header
