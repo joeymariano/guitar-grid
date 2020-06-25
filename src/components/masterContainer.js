c207c83681db84dcababbe462269cf719f0d3cca
joeymariano <joey.michalina.mariano@gmail.com>
2020-06-25 17:52:53 -0400

    added master container

---
+++ src/components/masterContainer.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react'
+import Strings from './strings'
+import Fretboard from './guitar/fretboard'
+import Header from './header'
+
+class MasterContainer extends Component {
+  constructor(props){
+  	super(props)
+  	this.state = { tuning: {guitar: ['E2', 'A2', 'D3', 'G3', 'B3', 'E4'],
+                            bass: ['E1', 'A1', 'D2', 'G2'],
+                            ukulele: ['G3', 'C3', 'E3', 'A3'],
+                            mandolin: ['D3', 'G3', 'A4', 'E4']} }
+  }
+
+  render() {
+    return (
+    	<div class='masterContainer'>
+        <Header />
+        <Fretboard insrtument={this.state.tuning.guitar}/>
+    	</div> 
+     )
+  }
+}
+
+export default MasterContainer
