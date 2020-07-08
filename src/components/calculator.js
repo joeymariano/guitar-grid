ff3ea5161808234c1a2b75aaf6507b463c65add7
joeymariano <joey.michalina.mariano@gmail.com>
2020-07-08 16:31:01 -0400

    starting form

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -2,7 +2,21 @@ import React from 'react'
 
 function Calculator(){
 	return( <div class="calculator">
-						CALC GOES HERE
+				    <form>
+				       <fieldset>
+				          <legend>&nbsp;Calculate&nbsp;</legend>
+				          <p>
+				             <label>Instrument </label>
+				             <select id = "instrumentList">
+				               <option value = "1">guitar</option>
+				               <option value = "2">bass</option>
+				               <option value = "3">ukulele</option>
+				               <option value = "4">mandolin</option>
+				             </select>
+				          </p>
+				          <button></button>
+				       </fieldset>
+				    </form>
 					</div> )
 }
 
