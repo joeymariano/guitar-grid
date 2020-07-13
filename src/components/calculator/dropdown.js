f2cf4ef24978dae4fe1ea5c8f4fa910f679f3e6b
joeymariano <joey.michalina.mariano@gmail.com>
2020-07-13 04:15:02 -0400

    moved tunings out of state and marked out where logic will happen

--- src/components/calculator/dropdown.js
+++ src/components/calculator/dropdown.js
@@ -5,11 +5,11 @@ function Dropdown(){
 	return( <div class='field-grid-item'>
 						<p>
 					  	<label>Instrument </label>
-					    <select id = "instrumentList">
-					      <option value = "1">guitar</option>
-					      <option value = "2">bass</option>
-					      <option value = "3">ukulele</option>
-					      <option value = "4">mandolin</option>
+					    <select id="instrumentList">
+					      <option value="1">guitar</option>
+					      <option value="2">bass</option>
+					      <option value="3">ukulele</option>
+					      <option value="4">mandolin</option>
 					    </select>
 					  </p> 
 				  </div> )
--- src/components/masterContainer.js
+++ src/components/masterContainer.js
@@ -7,21 +7,19 @@ import './masterContainer.css'
 class MasterContainer extends Component {
   constructor(props){
   	super(props)
-  	this.state = { tuning: {guitar: ['E2', 'A2', 'D3', 'G3', 'B3', 'E4'],
-                            bass: ['E1', 'A1', 'D2', 'G2'],
-                            ukulele: ['G3', 'C3', 'E3', 'A3'],
-                            mandolin: ['D3', 'G3', 'A4', 'E4']} }
+  	this.state = { instrument: 'guitar' }
   }
 
-  chooseInstrument = () => {
-
+  chooseInstrument = (instrument) => {
+    // get tuning from tunings.js
+    // compare (this) tuning state to what the dropdown string is in calculator
   }
 
   render() {
     return (
     	<div id='masterContainer'>
         <Header />
-        <Fretboard stringNames={this.state.tuning.ukulele}/>
+        <Fretboard stringNames={['E2', 'A2', 'D3', 'G3', 'B3', 'E4']}/>
         <Calculator />
     	</div> 
      )
