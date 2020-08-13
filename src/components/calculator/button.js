f1f8ef856e39a218ffdaa679757234376f70a8ee
joeymariano <joey.michalina.mariano@gmail.com>
2020-08-13 04:13:47 -0400

    toggle for button complete

--- src/components/calculator/button.js
+++ src/components/calculator/button.js
@@ -4,13 +4,19 @@ import './css/button.css'
 class Button extends Component {
 	constructor(props){
 		super(props)
+		this.state = { activated: false }
 
-		this.state = {  }
+		this.handleClick = this.handleClick.bind(this)
+	}
+
+	handleClick = (event) => {
+		event.preventDefault()
+		this.setState({ activated: this.state.activated = !this.state.activated })
 	}
 
 	render() {
 		return( <div class='field-grid-item'>
-							<button>
+							<button onClick={ this.handleClick }>
 							TEST BUTTON
 							</button>
 						</div> )
